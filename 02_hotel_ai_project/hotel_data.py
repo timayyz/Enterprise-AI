ca553600c86ec1b3f0b827fd0085ecf345c4da2a
timayyz <niuxiaotian666666@163.com>
2026-08-09 21:17:14 +0800

    organize hotel AI project structure

--- 02_hotel_ai_project/hotel_data.py
+++
@@ -1,7 +0,0 @@
-customers=["a","b","c"]
-ratings=[5,4,5]
-print("customers:",customers)
-print("ratings:",ratings)
-
-average_rating=sum(ratings)/len(ratings)
-print("average rating:",average_rating)
