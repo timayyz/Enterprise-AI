4176e5f5d257367d40e0ef348de2a2069dbb39c8
timayyz <niuxiaotian666666@163.com>
2026-08-09 20:16:36 +0800

    Create hotel AI learning project

---
+++ 01_python_learning/day1.py
@@ -0,0 +1,5 @@
+print("Hello,Niu")
+name= "Niu" 
+major= "hospitality Management"
+print("Hello",name)
+print("My major is ",major)
---
+++ 02_hotel_ai_project/hotel_data.py
@@ -0,0 +1,7 @@
+customers=["a","b","c"]
+ratings=[5,4,5]
+print("customers:",customers)
+print("ratings:",ratings)
+
+average_rating=sum(ratings)/len(ratings)
+print("average rating:",average_rating)
